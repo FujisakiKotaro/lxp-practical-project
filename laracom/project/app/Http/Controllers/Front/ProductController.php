7b80336565dd577c20e70c789c9fd5df75d06c39
FujisakiKotaroe <kotaro.fujisaki@g.dreamcareer.co.jp>
2024-03-01 10:44:52 +0900

    星の記述を修正

--- laracom/project/app/Http/Controllers/Front/ProductController.php
+++ laracom/project/app/Http/Controllers/Front/ProductController.php
@@ -7,10 +7,6 @@ use App\Shop\Products\Repositories\Interfaces\ProductRepositoryInterface;
 use App\Http\Controllers\Controller;
 use App\Shop\Products\Transformations\ProductTransformable;
 use App\Shop\Reviews\Review;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Psy\Readline\Hoa\Console;
 
 class ProductController extends Controller
 {
@@ -59,20 +55,17 @@ class ProductController extends Controller
         $images = $product->images()->get();
         $category = $product->categories()->first();
         $productAttributes = $product->attributes;
-
         $product_id = $product->id;
 
         //product_idから商品のレビューを取得
         $reviews = new Review();
         $reviews = $reviews->where('product_id', $product_id)->latest()->take(10)->get();
 
+        // 星の記述
         $stars = '';
-        foreach($reviews as $review){
-            $stars .= $this->generateStarRatingsHtml($review->rank);
+        foreach($reviews as $idx => $review){
+            $reviews[$idx]['stars'] = $this->generateStarRatingsHtml($review->rank);
         }
-
-        // var_dump($stars);
-        // echo $stars;
 
         return view('front.products.product', compact(
             'product',
@@ -85,22 +78,7 @@ class ProductController extends Controller
     }
 
     // 星評価のHTMLを生成するヘルパー関数
-    private function generateStarRatingsHtml($rank)
-    {
-        $starHtml = '';
-        for ($i = 0; $i < $rank; $i++) {
-            // $starHtml .= '<div style="color:#eef525;font-size:30px;">★</div>';
-            $starHtml .= '★';
-        }
-        for ($i = 0; $i < (5 - $rank); $i++) {
-            // $starHtml .= '<div style="color:#eef525;font-size:30px;">☆</div>';
-            $starHtml .= '☆';
-        }
-        // for ($i = 1; $i <= 5; $i++) {
-        //     // $starHtml .= '<div style="color:#eef525;font-size:30px;">' . ($i <= $rank ? '★' : '☆') . '</div>';
-        //     $starHtml .= $i <= $rank ? '★' : '☆';
-        // }
-        // return '<div style="display:flex; margin-top: 10px;">' . $starHtml . '</div>';
-        return $starHtml;
+    private function generateStarRatingsHtml($rank){
+        return str_repeat('★', $rank) . str_repeat('☆', 5 - $rank);
     }
 }
--- laracom/project/resources/views/layouts/front/product.blade.php
+++ laracom/project/resources/views/layouts/front/product.blade.php
@@ -80,13 +80,8 @@
                         <div class="review_list" style="display:flex;">
                             <div>
                             @foreach($reviews as $review)
-                                <div style="display:flex; margin-top: 10px;">
-                                    <!-- 星の記述 -->
-                                    @if(isset($review->$stars))
-                                    <div style="color:#eef525;font-size:30px;">{!! $review->$stars !!}</div>
-                                    @else
-                                    <div>starsがありません</div>
-                                    @endif
+                                <div style="margin-top: 20px;">
+                                    <div style="color:#eef525;font-size:30px;">{!! $review->stars !!}</div>
                                     <div class="product_explanation">{{$review->comment}}</div>
                                 </div>
                             @endforeach
